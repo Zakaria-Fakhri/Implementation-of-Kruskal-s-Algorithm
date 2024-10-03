2c64dfc55dea6d88f162c056aa255d998e1ad092
Zakariae Fakhri <zakafakhri_outb@outlook.de>
2024-10-03 07:48:40 +0200

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -55,35 +55,3 @@ def merge_sort(edges_list):
         edges_list[k] = right_half[j]
         j += 1
         k += 1
-
-
-
-        # Recursively sort the left and right halves
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        j = 0  # Index for right_half
-        k = 0  # Index for merged result
-        i = 0  # Index for left_half
-
-        # Merge the sorted halves back into edges_list
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i][2] < right_half[j][2]:  # Compare weights (the weight is located at index 2 of the tuple in edges_list)
-                edges_list[k] = left_half[i]  # Add the smaller element to the merged list
-                i += 1
-            else:
-                edges_list[k] = right_half[j]  # Add the smaller element to the merged list
-                j += 1
-            k += 1
-
-        # Add any remaining elements from the left half
-        while i < len(left_half):
-            edges_list[k] = left_half[i]
-            i += 1
-            k += 1
-
-        # Add any remaining elements from the right half
-        while j < len(right_half):
-            edges_list[k] = right_half[j]
-            j += 1
-            k += 1
